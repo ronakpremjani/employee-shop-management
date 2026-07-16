e5581cb22b6bb33955cd5b1e8e2412ef5e5f7976
Ronak <ronakpremjani8@email.com>
2026-07-16 11:52:45 +0530

    test

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: false,
 });
+
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
